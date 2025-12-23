4c84da5cf62698b3c60fe6ba643ca02099bcf0a9
MircaMircas <mikosmikus@gmail.com>
2025-12-23 20:12:01 +0100

    Add main files

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+include.exe sonic1.asm s1comb.asm
+
+REM ::: automatic recompression of data - disabled by default because it's slow
+REM ::: remove "REM" from the lines below to re-enable it
+REM derecmp.exe nc artnem_u artnem
+REM derecmp.exe kc artkos_u artkos
+REM derecmp.exe ec mapeni_u mapeni
+REM derecmp.exe ec map16_u map16
+REM derecmp.exe kc map256_u map256
+REM derecmp.exe ec sslay_u sslayout
+
+snasm68k.exe -emax 0 -p -o ae- s1comb.asm, s1built.bin
+rompad.exe s1built.bin 255 0
+fixheadr.exe s1built.bin
